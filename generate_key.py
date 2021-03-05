64818c0c4864f447f27e0d2affb811157cd83cb3
Owen <owen.shaule@gmail.com>
2021-03-05 09:35:43 -0500

    lol code works

--- generate_key.py
+++ generate_key.py
@@ -3,6 +3,6 @@ from cryptography.fernet import Fernet
 key = Fernet.generate_key()
 print(key)
 
-file = open('key.key', 'wb')
-file.write(key)
+file = open('key.key', 'w')
+file.write(key.decode())
 file.close()
